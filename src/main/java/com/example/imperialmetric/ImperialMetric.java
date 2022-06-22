4dd1f18dc7381baf7bc1d61f2f0e3897046b4466
Naveen Ram <naveen.ram2@yahoo.com>
2022-06-22 16:40:02 +0200

    add Exchange and ImperialMetric

--- src/main/java/com/example/imperialmetric/ImperialMetric.java
+++ src/main/java/com/example/imperialmetric/ImperialMetric.java
@@ -18,18 +18,6 @@ public class ImperialMetric {
         }
 
 
-          class StonetoKilo {
-            public static void main(String[] args) throws Exception {
-
-                BufferedReader bf = new BufferedReader(new
-                        InputStreamReader(System.in));
-                System.out.println("Enter the Stone:");
-                int Stone = Integer.parseInt(bf.readLine());
-                double Kilo = Stone * 6.35;
-                System.out.println("Kilo:" + Kilo);
-            }
-        }
-
 
     }
 }
---
+++ src/main/java/com/example/imperialmetric/calc.java
@@ -0,0 +1,4 @@
+package com.example.imperialmetric;
+
+public class calc {
+}
