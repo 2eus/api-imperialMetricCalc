8116d7a01c790d33a961bb4956a4a984146dafe8
Naveen Ram <naveen.ram2@yahoo.com>
2022-06-22 15:35:09 +0200

    Initial commits

---
+++ src/main/java/com/example/imperialmetric/ImperialMetricApplication.java
@@ -0,0 +1,13 @@
+package com.example.imperialmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialMetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperialMetricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imperialmetric/ImperialMetricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imperialmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialMetricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
